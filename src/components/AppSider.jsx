b2a96d1a85d55c032f7488136b59a4e7475a109d
emarishkin <ee.marishkin@yandex.ru>
2025-03-23 18:39:39 +0400

    3

--- src/components/AppSider.jsx
+++ src/components/AppSider.jsx
@@ -11,26 +11,31 @@ const siderStyle = {
     height:'100vh',  
   };
 
-  const data = [
-    'Racing car sprays burning fuel into crowd.',
-    'Japanese princess to wed commoner.',
-    'Australian walks 100km after outback crash.',
-    'Man charged over missing wedding girl.',
-    'Los Angeles battles huge wildfires.',
-  ];
-
 
 
 const AppSider = () =>{
 
 const [loading,setLoading] = useState(false)
+const [crypto,setCrypto] = useState([])
+const [assets,setAssets] = useState([])
 
 useEffect(()=>{
    async function preload(){
       setLoading(true)
-      await fakeFetchCrypto()
-      await fakeAssetsCrypto()
+      const crypto = await fakeFetchCrypto()
+      const assets = await fakeAssetsCrypto()
+     
       
+      setAssets(assets.map((asset)=>{
+        const coin = crypto.find((c)=>c.id === asset.id)
+        return{
+            grow: asset.price>coin.price,
+            totalAmound:asset.amount * coin.price,
+            totalProfit: asset.amount * coin.price - asset.amount * asset.price,
+            ...asset
+        }
+   }))
+      setCrypto(crypto)     
       setLoading(false)
     }
     preload()
@@ -43,41 +48,39 @@ useEffect(()=>{
 
     return(
         <Layout.Sider width="25%" style={siderStyle}>
-          <Card>
-          <Statistic
-          title="Active"
-          value={11.28}
-          precision={2}
-          valueStyle={{
-            color: '#3f8600',
-          }}
-          prefix={<ArrowUpOutlined />}
-          suffix="%"
-        />
-        <List
-        size='small'
-      bordered
-      dataSource={data}
-      renderItem={(item) => (
-        <List.Item>
-          <Typography.Text mark>[ITEM]</Typography.Text> {item}
-        </List.Item>
-      )}
-    />
-          </Card>
+          {assets.map(asset=>(
+            <Card>
+            <Statistic
+            title={asset.id}
+            value={11.28}
+            precision={2}
+            valueStyle={{color:asset.grow ? '#3f8600': 'red'}}
+            prefix={asset.grow ?<ArrowUpOutlined />: <ArrowDownOutlined />}
+            suffix="$"
+          />
+          <List
+          size='small'
+        bordered
+        dataSource={[
+            {title: 'Total profit',value:asset.totalProfit},
+            {title: 'Asset Amound',value:asset.amount},
+        ]}
+        renderItem={(item) => (
+            <List.Item>
+            <span>{item.title}</span>
+            <span>
+              {item.withTag && <Tag color={asset.grow? 'green':'red' }>{asset.growPercent}%</Tag>}
+            {item.isPlain && item.value}
+            {!item.isPlain && (
+              <Typography.Text type={asset.grow ? 'success': 'danger'}>{item.value}$</Typography.Text>)}
+          </span>
+          </List.Item>
+        )}
+      />
+            </Card>
+          ))}
 
-          <Card>
-          <Statistic
-          title="Idle"
-          value={9.3}
-          precision={2}
-          valueStyle={{
-            color: '#cf1322',
-          }}
-          prefix={<ArrowDownOutlined />}
-          suffix="%"
-        />
-          </Card>
+          
         </Layout.Sider>
     )
 }
